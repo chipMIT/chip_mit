cfbd22273dd051526d36240771d0f559c3133304
aditans <149258938+aditans@users.noreply.github.com>
2025-03-30 14:57:57 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    // Write C code here
+    if(fork()|| fork())
+    fork();
+printf("OS\n");
+
+    return 0;
+}
